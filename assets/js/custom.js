017f10b5b7fb0c024e62b1d7ecc8464abce1d7b1
carlos galicloud <carlos@galicloud.com>
2021-02-22 10:51:40 +0100

    canbiada fecha registro

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
 	});
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Mar 20, 2021 20:00:00").getTime();
+	var countDownDate = new Date("Mar 31, 2021 20:00:00").getTime();
 	calculateCountdown(countDownDate);
 // Update the count down every 1 second
 	var x = setInterval(function() {
